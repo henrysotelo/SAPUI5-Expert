48eac7560321dd84e1d180514dfb0c07b7e38d5a
empresariosotelo@hotmail.com <empresariosotelo@hotmail.com>
2022-04-26 21:38:42 +0000

    Commit 2 Elementos y Controladores UI5

--- logali/employees/webapp/controller/MainView.controller.js
+++ logali/employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,5 @@
+//import { get } from "http";
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
@@ -10,6 +12,19 @@ sap.ui.define([
         return Controller.extend("logaligroup.employees.controller.MainView", {
             onInit: function () {
 
+            },
+
+            onValidate: function(){
+                var inputEmployees = this.byId("inputEmployees");
+                var valueEmployees = inputEmployees.getValue();
+                
+                if(valueEmployees.length === 6){
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true)
+                }else{
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
             }
         });
     });
